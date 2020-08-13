7d6dd73be843e444afcfac07126931664e0f788b
julian190 <68065877+julian190@users.noreply.github.com>
2020-08-13 04:21:00 +0300

    done with navbar

--- js/app.js
+++ js/app.js
@@ -17,9 +17,10 @@
  * Define Global Variables
  *
 */
-const nav = document.querySelector(".navbar__menu");
+const nav_bar = document.querySelector(".navbar__menu");
 const sections = document.querySelectorAll(".landing__container");
-const ul = document.createElement("ul");
+const un_order_list = document.createElement("ul");
+let list_of_offsets = []
 
 
 
@@ -28,7 +29,15 @@ const ul = document.createElement("ul");
  * Start Helper Functions
  *
 */
-
+var isInViewport = function (elem) {
+	var distance = elem.getBoundingClientRect();
+	return (
+		distance.top >= 0 &&
+		distance.left >= 0 &&
+		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+	);
+};
 
 
 /**
@@ -36,21 +45,42 @@ const ul = document.createElement("ul");
  * Begin Main Functions
  *
 */
-
+function add_sections_to_nav()
+{
     for (section of sections)
     {
-      let x = document.createElement("li")
-      x.innerText = section.firstElementChild.innerText;
-      ul.appendChild(x);
-      nav.appendChild(ul);
+        // build the nav
+      let list_item = document.createElement("li");
+      let anchor_tag = document.createElement("a");
+      anchor_tag.href = "#"+section.parentNode.id;
+      anchor_tag.classList.add("menu__link");
+      anchor_tag.innerHTML = section.firstElementChild.innerText;
+      list_item.appendChild(anchor_tag)
+      un_order_list.appendChild(list_item);
+      nav_bar.appendChild(un_order_list);
     }
+}
 
 
-// build the nav
 
 
 // Add class 'active' to section when near top of viewport
+function get_offsets()
+{
 
+    for (section of sections)
+    {
+        list_of_offsets.push(section.parentElement.offsetTop);
+    }
+}
+var findMe = document.querySelector('.main__hero');
+window.addEventListener('scroll', function (event) {
+	if (isInViewport(findMe)) {
+		console.log('In viewport!');
+	} else {
+    console.log('Nope...');
+  }
+}, false);
 
 // Scroll to anchor ID using scrollTO event
 
@@ -62,7 +92,8 @@ const ul = document.createElement("ul");
 */
 
 // Build menu
-
+add_sections_to_nav()
 // Scroll to section on link click
 
 // Set sections as active
+get_offsets()
